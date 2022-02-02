745b8f92f939f8ed093e9aa1ed212baa42855884
baptiste carbillet <baptistecarbillet@MacBook-Pro-de-baptiste.local>
2022-02-02 16:58:02 +0100

    class

--- class.py
+++ class.py
@@ -1,21 +1,37 @@
 class Employee:
-    def __init__(self, Name, Latitude, Longitude, Skill, Level, WorkingStartTime, WorkingEndTime):
-        self.Name = Name
-        self.Latitude = Latitude
-        self.Longitude = Longitude
-        self.Skill = Skill
-        self.Level = Level
-        self.WorkingStartTime = WorkingStartTime
-        self.WorkingEndTime = WorkingEndTime
+    def __init__(self, name, latitude, longitude, skill, level, starttime, endtime):
+        self.name = name
+        self.latitude = latitude
+        self.longitude = longitude
+        self.skill = skill
+        self.level = level
+        self.starttime = starttime
+        self.endtime = endtime
     
 class Task:
     
-    def  __init(self, TaskId, Latitude, Longitude, TaskDuration, Skill, Level, OpeningTime, ClosingTime):
-        self.TaskId = TaskId
-        self.Latitude = Latitude
-        self.Longitude = Longitude
-        self.Taskduration = TaskDuration
-        self.Skill = Skill
-        self.Level = Level
-        self.OpeningTime = OpeningTime
-        self.ClosingTime = ClosingTime
+    def  __init(self, id, latitude, longitude, duration, skill, level, openingtime, closingtime):
+        self.id = id
+        self.latitude = latitude
+        self.longitude = longitude
+        self.duration = duration
+        self.Skill = skill
+        self.Level = level
+        self.openingtime = openingtime
+        self.closingtime = closingtime
+
+class Employees_Unavailabilities:
+    def __init__(self,name, latitude, longitude,start,end):
+        self.name = name
+        self.latitude = latitude
+        self.longitude = longitude
+        self.start = start
+        self.end = end
+
+class Task_Unavailabilities:
+    def __init__(self,id, start, end):
+        self.id = id
+        self.start = start
+        self.end = end
+        
+        
